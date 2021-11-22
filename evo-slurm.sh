9dcd76c497232f5da89fe32d2e59e458873c1ac5
Recep Can Altınbağ <recepcanaltinbag@gmail.com>
2021-11-22 17:59:26 +0300

    created file

---
+++ evo-slurm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p palamut-cude
+#SBATCH -A raltinbag
+#SBATCH -J evo_run
+#SBATCH -o output.out
+#SBATCH --gres=gpu:1
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --cpus-per-task 1
+#SBATCH --time=0:5:00
+
+singularity run --nv evobio_v1.sif 
