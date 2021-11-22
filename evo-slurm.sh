6b72b91c8b5b251c78d48c5fbbc7bfd5c836c237
Recep Can Altınbağ <recepcanaltinbag@gmail.com>
2021-11-22 18:15:29 +0300

    Update evo-slurm.sh

--- evo-slurm.sh
+++ evo-slurm.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#SBATCH -p akya-cuda
+#SBATCH -p sardalya
 #SBATCH -A raltinbag
 #SBATCH -J evo_run
 #SBATCH -o output.out
-#SBATCH --gres=gpu:1
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --cpus-per-task 1
-#SBATCH --time=0:5:00
+#SBATCH --time=0:10:00
 
 singularity run --nv evobio_v1.sif 
