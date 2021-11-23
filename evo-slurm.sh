a093382ca0114fbd017c53bf71b1d4e48df9b53f
Recep Can Altınbağ <recepcanaltinbag@gmail.com>
2021-11-23 07:34:14 +0300

    Update evo-slurm.sh

--- evo-slurm.sh
+++ evo-slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -p short
+#SBATCH -p hamsi
 #SBATCH -A raltinbag
 #SBATCH -J evo_run
 #SBATCH -o output.out
 #SBATCH --time=1:00:00
 
-singularity run --nv evobio_v1.sif 
+singularity run evobio_v1.sif 
